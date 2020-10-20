63dce6c457c125bbfdd48ac785b36b16000a3b4a
m2 <467057463@qq.com>
2020-10-20 22:26:06 +0800

    study controller

---
+++ src/articles/articles.module.ts
@@ -0,0 +1,6 @@
+import { Module } from "@nestjs/common";
+import { ArticlesController } from './articles.controller';
+@Module({
+  controllers: [ArticlesController]
+})
+export class ArticlesModule{}
