042ed9c4e6110a162a41882b4ea4bf71addd7ef0
m2 <467057463@qq.com>
2020-10-21 17:50:48 +0800

    study controller

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,9 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { All, Controller, Get, HttpCode, Param, Post, Redirect, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('articles')
 export class ArticlesController {
   @Get()
-  findAll(): string {
-    return 'articles'
+  @Redirect('https://www.baidu.com', 301)
+  findAll(
+    @Req() request: Request,
+  ): any {
+    return {
+      url: 'https://www.163.com',
+      code: 302
+    }
+  }
+
+  @Post()
+  create(): string{
+    return 'create success'
+  }
+
+  @Get(':id')
+  findOne(
+    @Param('id') id
+  ):string{
+    return id
   }
 }
