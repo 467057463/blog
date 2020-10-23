e8bc9f976f93e28ee2d91ab0cc4567e6d25df95f
m2 <467057463@qq.com>
2020-10-23 23:05:34 +0800

    study ts class

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,8 +1,31 @@
-import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 import { ArticlesService } from './articles.service';
 import { ArticleDto } from './articles.dto';
 import { Article } from './interfaces/article.interface';
+
+
+class Animal {
+  // public name = 'mm';
+  readonly x = 1;
+
+  constructor(public readonly name){
+    // this.name = name;
+  }
+  getX(){
+    return this.x;
+  }
+  setX(){
+    this.x = '1234'
+  }
+}
+
+class Cat extends Animal{
+  getX(){
+    return this.x;
+  }
+}
+
 
 @Controller('articles')
 export class ArticlesController {
@@ -12,6 +35,10 @@ export class ArticlesController {
 
   @Get()
   async findAll(): Promise<Article[]>{
+    console.log(Animal)
+    const a = new Animal('mm')
+    a.x = 1;
+    a.x;
     return this.articlesService.findAll()
   }
 
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -13,3 +13,4 @@ export class ArticlesService {
     return this.articles;
   }
 }
+
