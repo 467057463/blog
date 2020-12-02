8d475332eff5fbad9ac3fcf8949838d4462f3333
m2 <467057463@qq.com>
2020-12-02 20:55:03 +0800

    change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'blog_api',
       script: 'npm',
-      args: 'start:prod',
+      args: 'run start:prod',
       env: {
         "COMMON_VARIABLE": "true"
       },
